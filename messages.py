f79bd40fb2802bf5cd5039e1dfbf6ec717f1575c
Lucas Zacchi de Medeiros <lzacchi@users.noreply.github.com>
2017-05-22 16:15:27 -0300

    Add files via upload

---
+++ messages.py
@@ -0,0 +1,34 @@
+"""Messages used by planetinha_facts_bot."""
+start_msg = "Prepare-se para conhecer o fantabulástico mundo dos planetinhas"
+
+help_msg = 'Envie "/fact" ou seja surpreendido'
+
+help_msg = 'Você está prestes a mergulhar em um universo fantabulástico!'
+
+facts = ["Um dia em Júpiter dura 8 horas.",
+         "Vênus possui apenas 2 dias em um ano.",
+         "A densidade de Saturno é 0.7 g/cm³. É o único planeta que boiaria na água",
+         "Todos os planetas caberiam na distância entre a Terra e a Lua.",
+         "a 'Mancha Vermelha', em Júpiter, é uma Tempestade com diâmetro maior que o da Terra",
+         "Urano possui os ventos mais rápidos do Sistema Solar, podendo chegar a 900km/h",
+         "Marte é o único planeta habitado inteiramente por robôs",
+         "Existem 181 luas descobertas no Sistema Solar",
+         "Plutão é menor do que os Estados Unidos",
+         "99.86% da massa do sistema solar está contida no Sol",
+         "1 milhão de planetas Terra caberiam dentro do Sol",
+         "Mercúrio e Vênus possuem fases como as da Lua",
+         "Mercúrio faz 3 rotações a cada 2 translações",
+         "A terra é o planeta mais denso, com 5.515 g/cm³"]
+
+photos = ['https://s-media-cache-ak0.pinimg.com/originals/13/fd/40/13fd40e830fa3c00c4638f743cbce6a5.jpg',  # Pale Blue Dot
+          'https://upload.wikimedia.org/wikipedia/commons/3/3d/Uranus2.jpg',  # Uranus
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Neptune_Full.jpg/1200px-Neptune_Full.jpg',  # Neptune
+          'https://www.nasa.gov/centers/goddard/images/content/638831main_globe_east_2048.jpg',
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg/1200px-Mercury_in_color_-_Prockter07-edit1.jpg',  # Mercury
+          'https://upload.wikimedia.org/wikipedia/commons/8/85/Venus_globe.jpg',  # Venus
+          'https://www.nasa.gov/sites/default/files/thumbnails/image/jupapr3color-jd-170304.png',  # Earth
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Saturn_during_Equinox.jpg/1200px-Saturn_during_Equinox.jpg',  # Saturn
+          'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/1200px-OSIRIS_Mars_true_color.jpg',  # Mars
+          'https://upload.wikimedia.org/wikipedia/commons/a/a7/Pluto-01_Stern_03_Pluto_Color_TXT.jpg']  # Pluto
+
+TOKEN = '386613898:AAEim14KAq2RCMjfv2QJOuhYbETz1disTHo'
---
+++ planetinha_facts.py
@@ -0,0 +1,59 @@
+"""Um bot de Telegram que envia fun facts sobre o Sistema Solar."""
+import logging
+
+# import os
+
+import random
+
+from messages import facts, help_msg, start_msg, TOKEN, photos
+
+# import telegram
+
+from telegram.ext import CommandHandler, Updater
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+def start(bot, update):
+    """Responde ao comando '/start'."""
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text=start_msg)
+
+
+# def trigger(percentage=5):
+#     return random.randrange(100) < percentage
+
+
+def help_cmd(bot, update):
+    """Responde ao comando '/help'."""
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text=help_msg)
+
+
+def fun_facts(bot, update):
+    """Responde ao comando '/fact', e envia um dos fatos da lista."""
+    bot.sendMessage(chat_id=update.message.chat_id,
+                    text=random.choice(facts))
+
+
+def fun_pics(bot, update):
+    """Responde ao comando '/pic', e envia uma das imagens da lista."""
+    bot.send_photo(chat_id=update.message.chat_id,
+                   photo=random.choice(photos))
+
+
+token = TOKEN
+updater = Updater(token)
+dispatcher = updater.dispatcher
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+help_handler = CommandHandler('help', help_cmd)
+dispatcher.add_handler(help_handler)
+facts_handler = CommandHandler('fact', fun_facts)
+dispatcher.add_handler(facts_handler)
+pic_handler = CommandHandler('pic', fun_pics)
+dispatcher.add_handler(pic_handler)
+
+updater.start_polling()
+updater.idle()
