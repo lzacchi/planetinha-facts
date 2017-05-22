2944b4169fd58c739d18abef7e7148c33f479ccd
Lucas <zacchilucasm@gmail.com>
2017-05-22 17:04:36 -0300

    More facts and pictures

--- messages.py
+++ messages.py
@@ -1,9 +1,9 @@
 """Messages used by planetinha_facts_bot."""
 start_msg = "Prepare-se para conhecer o fantabulástico mundo dos planetinhas"
 
-help_msg = 'Envie "/fact" ou seja surpreendido'
+help_msg = 'Envie "/fact"'
 
-help_msg = 'Você está prestes a mergulhar em um universo fantabulástico!'
+help_msg = 'Você está prestes a mergulhar em um universo cheio de conhecimento'
 
 facts = ["Um dia em Júpiter dura 8 horas.",
          "Vênus possui apenas 2 dias em um ano.",
@@ -21,7 +21,12 @@ facts = ["Um dia em Júpiter dura 8 horas.",
          "A terra é o planeta mais denso, com 5.515 g/cm³",
          "Júpiter irradia mais calor do que recebe do Sol",
          "Saturno possui 62 luas. Apenas 53 foram oficialmente nomeadas",
-         "Titã é uma lua de Saturno, e a única no Sistema Solar a possuir uma atmosfera"]
+         "Titã é uma lua de Saturno, e a única no Sistema Solar a possuir uma atmosfera",
+         "A temperatura média na superfície de Vênus é 462ºC. É o planeta mais quente no Sistema Solar",
+         "Vênus é o terceiro objeto mais brilhante no céu",
+         "Só é possível observar Vênus em dois momentos: Logo'após o Sol se por, ou logo antes do Sol nascer.",
+         "O por do Sol em Marte é azul"]
+
 
 photos = ['https://s-media-cache-ak0.pinimg.com/originals/13/fd/40/13fd40e830fa3c00c4638f743cbce6a5.jpg',  # Pale Blue Dot
           'https://upload.wikimedia.org/wikipedia/commons/3/3d/Uranus2.jpg',  # Uranus
@@ -32,6 +37,9 @@ photos = ['https://s-media-cache-ak0.pinimg.com/originals/13/fd/40/13fd40e830fa3
           'https://www.nasa.gov/sites/default/files/thumbnails/image/jupapr3color-jd-170304.png',  # Earth
           'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Saturn_during_Equinox.jpg/1200px-Saturn_during_Equinox.jpg',  # Saturn
           'https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/1200px-OSIRIS_Mars_true_color.jpg',  # Mars
-          'https://upload.wikimedia.org/wikipedia/commons/a/a7/Pluto-01_Stern_03_Pluto_Color_TXT.jpg']  # Pluto
+          'https://upload.wikimedia.org/wikipedia/commons/a/a7/Pluto-01_Stern_03_Pluto_Color_TXT.jpg',  # Pluto
+          'https://www.geek.com/wp-content/uploads/2014/06/venus-2.jpg'  # Venus' surface
+          'https://www.jpl.nasa.gov/images/msl/20150508b/pia19400-16.jpg'  # Mars' sunset
+          ]
 
 TOKEN = '386613898:AAEim14KAq2RCMjfv2QJOuhYbETz1disTHo'
